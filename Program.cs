45dfcd8d79bdbe69e797c2fc35025251e28c07d8
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-06 14:39:46 -0500

    syntax exercise

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+int  answer =4;
+string feedback;
+
+if (answer < 9)
+{
+    feedback = answer + "less than nine";
+}
+else
+{
+    feedback = answer + "greater than or equal to nine";
+
+}
+Console.WriteLine(feedback);
